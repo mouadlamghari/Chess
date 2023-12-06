58b26bdadfdc2e0d3b682f315e8fe2231fa89e0b
mouadLLL <mouaddev0002@gmail.com>
2023-12-06 14:03:42 +0100

    r

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import PlayWithFriend from './Pages/PlayWithFriend'
 import Chose from './Pages/Chose'
-import PlayOnline from './Pages/playOnline'
-import CreateRoom from './Pages/CreateRoom'
+
 
 const Home = () => {
   return (
