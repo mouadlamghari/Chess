e0da2c4e388750edad797f507f1fc7d86115d2dc
mouadlamghari <142666513+mouadlamghari@users.noreply.github.com>
2024-06-23 18:14:58 +0100

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,9 +5,11 @@ import Chose from './Pages/Chose'
 
 
 const Home = () => {
+  console.log('rendred')
   return (
     <div>
       MOUAD LAMGHARI
+      <Chose/>
     <Routes>
         <Route path='/' element={<Chose/>} />
         <Route path='/playWithFriend' element={<PlayWithFriend/>} />
